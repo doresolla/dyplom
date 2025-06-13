e6621c9d57fff850c122141233727cf2acd9827e
doresolla <dondupova02@mail.ru>
2025-06-13 09:33:51 +0700

    models

--- website/views.py
+++ website/views.py
@@ -11,6 +11,7 @@ from django.db.models import Avg
 from .mainAction import generate_summary
 
 from .forms import VideoTagForm, SummaryReviewForm, UserRegistrationForm, LoginForm, VideoUploadForm,SummaryAlgoFormatForm
+from .models import Summary, Video, Audio, SummaryReview, User, VideoOwnership, Tag, VideoTag
 from .tasks import run_summary_task
 
 from website.video import download, get_video_duration, extract_thumbnail, PROXIES, read_file, check_title
