a8d6975c146add599221e0e72e1d394d09df5a3e
doresolla <dondupova02@mail.ru>
2025-06-23 06:35:27 +0700

    # -*- coding: utf-8 -*-

--- processing/tasks.py
+++ processing/tasks.py
@@ -1,5 +1,4 @@
-# processing/tasks.py
-
+# -*- coding: utf-8 -*-
 from celery import shared_task
 from django.core.mail import send_mail
 from .models import ProcessingRequest
@@ -9,9 +8,9 @@ from .models import ProcessingRequest
 def processing_request_created(request_id):
     request_obj = ProcessingRequest.objects.get(id=request_id)
 
-    subject = f"Запрос на обработку видео №{request_obj.id}"
-    message = f"Здравствуйте, {request_obj.user.username}!\n\n" \
-              f"Ваш запрос на обработку видео '{request_obj.video.title}' принят в обработку.\n" \
-              f"Дата создания: {request_obj.requested_at.strftime('%d.%m.%Y %H:%M')}."
+    subject = f"Р—Р°РїСЂРѕСЃ РЅР° РѕР±СЂР°Р±РѕС‚РєСѓ РІРёРґРµРѕ в„–{request_obj.id}"
+    message = f"Р—РґСЂР°РІСЃС‚РІСѓР№С‚Рµ, {request_obj.user.username}!\n\n" \
+              f"Р’Р°С€ Р·Р°РїСЂРѕСЃ РЅР° РѕР±СЂР°Р±РѕС‚РєСѓ РІРёРґРµРѕ '{request_obj.video.title}' РїСЂРёРЅСЏС‚ РІ РѕР±СЂР°Р±РѕС‚РєСѓ.\n" \
+              f"Р”Р°С‚Р° СЃРѕР·РґР°РЅРёСЏ: {request_obj.requested_at.strftime('%d.%m.%Y %H:%M')}."
 
     send_mail(subject, message, 'admin@project.com', [request_obj.user.email])
