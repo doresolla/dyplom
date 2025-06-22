8060bdba9d8f8a8a069508acf6a1d3b7f178f291
doresolla <dondupova02@mail.ru>
2025-06-23 05:59:48 +0700

    add algo_form

--- processing/views.py
+++ processing/views.py
@@ -3,30 +3,40 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProcessingRequestForm
 from .models import ProcessingRequest
 from website.models import Video, Audio, Summary, User
+from website.forms import SummaryAlgoFormatForm, VideoUploadForm
 
 
 def create_processing_request(request, video_id):
     user = User.objects.get(pk=request.session.get('user_id'))
+    if not user:
+        return redirect('login_user')  # если пользователь не авторизован
+
     video = get_object_or_404(Video, pk=video_id)
     audio = Audio.objects.filter(video=video).first()
 
+    algo_form = SummaryAlgoFormatForm()
+    format_form = VideoUploadForm()
+
     if request.method == 'POST':
-        form = ProcessingRequestForm(request.POST)
-        if form.is_valid():
-            processing_request = form.save(commit=False)
-            processing_request.user = user
-            processing_request.video = video
-            processing_request.audio = audio
-            processing_request.save()
-
-            # Здесь можно запустить обработку
-            # run_summary_task.delay(audio.id, ...)
-
-            return render(request, 'success.html', {'request_obj': processing_request})
+        algo_form = SummaryAlgoFormatForm(request.POST)
+        if algo_form.is_valid():
+            processing_request = ProcessingRequest.objects.create(
+                user=user,
+                video=video,
+                audio=audio,
+                algorithm=algo_form.cleaned_data['algo'],
+                format=algo_form.cleaned_data['format'],
+                ratio=algo_form.cleaned_data['ratio']
+            )
+            # после оформления можно запустить обработку
+            # run_summary_task.delay(audio.id, algo=..., format=..., ratio=...)
+            return render(request, 'success.html',
+                          {'request_obj': processing_request, 'user_name': user.username})
     else:
         form = ProcessingRequestForm()
 
     return render(request, 'create.html', {
-        'form': form,
-        'video': video
+        'video': video,
+        'form': algo_form,
+        'user_name': user.username,
     })
